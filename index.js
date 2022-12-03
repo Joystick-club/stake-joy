1f5443411c816dd3cdf32bba515e45016e791efc
Corbin <amalektricity@joystick.club>
2022-12-03 12:55:05 +0700

    Display total joy deposited

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const StakingRewards = () => {
   // State variables to store the form inputs
   const [amount, setAmount] = useState('');
 
-  // State variable to store the total rewards earned by the user
+  // State variables to store the total rewards and JOY deposited by the user
   const [totalRewards, setTotalRewards] = useState(0);
+  const [totalDeposited, setTotalDeposited] = useState(0);
 
   // Hook to access the user's web3 provider
   const { accounts, library } = useWeb3();
@@ -45,7 +46,29 @@ const StakingRewards = () => {
       }
     };
 
+    const getTotalDeposited = async () => {
+      // Ensure that the user is connected and has an account
+      if (!accounts || accounts.length === 0) {
+        return;
+      }
+
+      // Get the user's account
+      const userAccount = accounts[0];
+
+      try {
+        // Call the contract's view function to get the total JOY deposited by the user
+        const totalDeposited = await contract.stakedBalances(userAccount);
+
+        // Update the state
+        setTotalDeposited(totalDeposited);
+      } catch (error) {
+        // Handle any errors
+        console.error(error);
+      }
+    };
+
     getTotalRewards();
+    getTotalDeposited();
   }, [accounts, library]);
 
   // Function to handle form submission
@@ -80,7 +103,6 @@ const StakingRewards = () => {
       console.error(error);
     }
   };
-
 
   // Function to handle withdrawal of JOY tokens
   const handleWithdraw = async () => {
@@ -158,6 +180,9 @@ const StakingRewards = () => {
       <button onClick={handleClaimRewards}>Claim Rewards</button>
       <p>
         Total Rewards Earned: {totalRewards}
+      </p>
+      <p>
+        Total JOY Deposited: {totalDeposited}
       </p>
     </div>
   );
